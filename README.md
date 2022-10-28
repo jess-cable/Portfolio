7efc2f40a549b13ff48652b72e428713fcc4e2f6
Kimberly Cable <cableredi@comcast.net>
2022-10-28 17:17:12 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Welcome to my Portfolio
+
+## Summary
+Here you will find projects that I have done and the skills that I have.
+
+## [Check it out...](https://cableredi.github.io/kcportfolio/)
+
+## Technologies Used
+HTML, SCSS, JavaScript, jQuery
---
+++ assets/scripts/scripts.js
@@ -0,0 +1,38 @@
+$(document).ready(function() {
+    var folder = "./assets/images/home/";
+
+    $.ajax({
+        url : folder,
+        success: function (data) {
+            $(data).find("a").attr("href", function (i, val) {
+                if( val.match(/\.(jpe?g|png|gif)$/) ) {
+                    $(".main__projects-contents").append( "<img src='"+ val +"'>" );
+                } 
+            });
+        }
+    });
+
+    /* Modal */
+    let modal = document.getElementsByClassName("main__selectedprojects-project-modal");
+    let btn = document.getElementsByClassName("main__selectedprojects-project-info");
+    let span = document.getElementsByClassName(
+    "main__selectedprojects-project-modal-content-close"
+    );
+
+    for (let i = 0; i < modal.length; i++) {
+    btn[i].onclick = function () {
+        modal[i].style.display = "block";
+    };
+
+    span[i].onclick = function () {
+        modal[i].style.display = "none";
+    };
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+    };
+})
