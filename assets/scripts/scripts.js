2836c2d9423cad56b91c230331c22d6266fcfea5
Kimberly Cable <cableredi@comcast.net>
2023-01-19 16:15:06 -0700

    redesign

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -1,25 +1,27 @@
-$(document).ready(function() {
-    /* Modal */
-    let modal = document.getElementsByClassName("main__selectedprojects-project-modal");
-    let btn = document.getElementsByClassName("main__selectedprojects-project-info");
-    let span = document.getElementsByClassName(
-    "main__selectedprojects-project-modal-content-close"
-    );
+// Get each modal and close button
+const triggers = document.getElementsByClassName("trigger");
+const triggerArray = Array.from(triggers).entries();
+const modals = document.getElementsByClassName("modal");
+const closeButtons = document.getElementsByClassName("close-button");
 
-    for (let i = 0; i < modal.length; i++) {
-    btn[i].onclick = function () {
-        modal[i].style.display = "block";
-    };
+let modal01 = document.getElementById('modal01');
+let modal02 = document.getElementById('modal02');
 
-    span[i].onclick = function () {
-        modal[i].style.display = "none";
-    };
-    }
+// Then use `for...of`-loop with the index of each item in `triggerArray` for listening to a click event which toggles each modal to open and close
+for (let [index, trigger] of triggerArray) {
+  function toggleModal() {
+    modals[index].classList.toggle("show-modal");
+  };
 
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function (event) {
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
-    };
-})
+  trigger.addEventListener("click", toggleModal);
+  closeButtons[index].addEventListener("click", toggleModal);
+}
+
+window.onclick = function(event) {
+  if (event.target == modal01) {
+    modal01.style.display = "none";
+  }
+  if (event.target == modal02) {
+    modal02.style.display = "none";
+  }
+}
